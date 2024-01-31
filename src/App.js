8cf66cd6670b50bc757aadaaf1499db5d8a1eafa
goodalways <jeremy.yap27@gmail.com>
2024-01-31 14:14:49 +0800

    testing images

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import logo from './logo.svg';
+import sixjob from './6thjob.png';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={sixjob} className="Six-JobLogo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
